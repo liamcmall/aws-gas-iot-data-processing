0e09920ab3ec4eed5bbee01dff7a9d8ef477117d
liam mall <29150314+liamcmall@users.noreply.github.com>
2024-03-04 19:31:18 +0000

    updating the input to rds

--- lambda/insert-data-to-rds.py
+++ lambda/insert-data-to-rds.py
@@ -1,24 +1,23 @@
 import psycopg2
 import os
+import json
 
 def lambda_handler(event, context):
-    # Database connection parameters
-    conn = psycopg2.connect(
-        dbname=os.environ['DB_NAME'], 
-        user=os.environ['DB_USER'], 
-        password=os.environ['DB_PASSWORD'], 
-        host=os.environ['DB_HOST']
-    )
+    
+    records = json.loads(event['body'])['records']
+    
+    conn = psycopg2.connect(dbname=os.environ['DB_NAME'], user=os.environ['DB_USER'], password=os.environ['DB_PASSWORD'], host=os.environ['DB_HOST'])
     cur = conn.cursor()
     
-    # Example of inserting a single data record
-    sensor_id = event['sensor_id']
-    value = event['value']
-    measurement_type = event['measurement_type']
-    timestamp = event['timestamp']
+    insert_query = """
+    INSERT INTO sensor_data (time, R1, R2, R3, R4, R5, R6, R7, R8, temperature, humidity)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
     
-    cur.execute("INSERT INTO sensor_data (sensor_id, value, measurement_type, timestamp) VALUES (%s, %s, %s, %s)",
-                (sensor_id, value, measurement_type, timestamp))
+    for record in records:
+        
+        data = (record['time'], record['R1'], record['R2'], record['R3'], record['R4'], record['R5'], record['R6'], record['R7'], record['R8'], record['Temp.'], record['Humidity'])
+        cur.execute(insert_query, data)
     
     conn.commit()
     cur.close()
@@ -28,4 +27,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'Successfully inserted data into RDS'
     }
-    
