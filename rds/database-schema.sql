25e961b91580beb705256d39f46fad9e094770e6
liam mall <29150314+liamcmall@users.noreply.github.com>
2024-03-04 19:30:11 +0000

    update the schema of the database

--- rds/database-schema.sql
+++ rds/database-schema.sql
@@ -1,7 +1,14 @@
 CREATE TABLE sensor_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sensor_id VARCHAR(255),
-    value DECIMAL(10, 2),
-    measurement_type VARCHAR(50),
-    timestamp TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP,
+    R1 DECIMAL(10, 2),
+    R2 DECIMAL(10, 2),
+    R3 DECIMAL(10, 2),
+    R4 DECIMAL(10, 2),
+    R5 DECIMAL(10, 2),
+    R6 DECIMAL(10, 2),
+    R7 DECIMAL(10, 2),
+    R8 DECIMAL(10, 2),
+    temperature DECIMAL(5, 2),
+    humidity DECIMAL(5, 2)
 );
